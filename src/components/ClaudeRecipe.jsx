afd02966d3012dde434eae294d366d6839bedd9a
Yen Tran <yentran@Yens-MBP.lan>
2025-01-07 10:34:43 -1000

    cleanup

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -1,7 +1,9 @@
 import ReactMarkdown from 'react-markdown'
+
 export default function ClaudeRecipe({ recipe }) {
   return (
-    <section className='suggested-recipe-container'>
+    <section className='suggested-recipe-container' aria-live='polite'>
+      <h2>Chef Claude Recommends:</h2>
       <ReactMarkdown>{recipe}</ReactMarkdown>
     </section>
   )
