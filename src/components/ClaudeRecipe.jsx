c99328010765509a731e22cefb3d2d79fe6bae6e
Yen Tran <yentran@Yens-MBP.lan>
2025-01-07 10:33:08 -1000

    add ReactMarkdown

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -1,7 +1,8 @@
+import ReactMarkdown from 'react-markdown'
 export default function ClaudeRecipe({ recipe }) {
   return (
-    <section>
-      {recipe}
+    <section className='suggested-recipe-container'>
+      <ReactMarkdown>{recipe}</ReactMarkdown>
     </section>
   )
 }
