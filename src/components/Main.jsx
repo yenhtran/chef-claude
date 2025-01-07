dcdb89d9de0747857652d75c68887d629b63e536
Yen Tran <yentran@Yens-MBP.lan>
2025-01-06 16:12:52 -1000

    update to show get-recipe after 4 ingredients added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -25,19 +25,20 @@ export default function Main() {
         />
         <button>Add ingredient</button>
       </form>
-      { ingredientListItems.length > 0 &&
+
         <section>
         <h2>Ingredients on hand:</h2>
-        <ul className='ingredients-list'>{ingredientListItems}</ul>
-        <div className='get-recipe-container'>
-          <div>
-            <h3>Ready for a recipe?</h3>
-            <p>Generate a recipe from your list of ingredients</p>
+        <ul className='ingredients-list' aria-live="polite">{ingredientListItems}</ul>
+        { ingredients.length > 3 &&
+          <div className='get-recipe-container'>
+            <div>
+              <h3>Ready for a recipe?</h3>
+              <p>Generate a recipe from your list of ingredients</p>
+            </div>
+            <button>Get a recipe</button>
           </div>
-          <button>Get a recipe</button>
-        </div>
+        }
         </section>
-      }
     </main>
   )
 }
