a604f6458bf59977c341e921e180b5b3e8a51bc4
Yen Tran <yentran@Yens-MBP.lan>
2025-01-06 16:01:52 -1000

    add get-recipe container

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -25,9 +25,19 @@ export default function Main() {
         />
         <button>Add ingredient</button>
       </form>
-      <ul>
-        {ingredientListItems}
-      </ul>
+      { ingredientListItems.length > 0 &&
+        <section>
+        <h2>Ingredients on hand:</h2>
+        <ul className='ingredients-list'>{ingredientListItems}</ul>
+        <div className='get-recipe-container'>
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients</p>
+          </div>
+          <button>Get a recipe</button>
+        </div>
+        </section>
+      }
     </main>
   )
 }
